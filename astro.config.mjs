0877989c8b23f1586db34225d9f922e2f5f8f72e
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-05-31 18:04:22 +0530

    fix: bring back custom domain as site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nagariahussain.github.io',
+  site: 'https://buildwithhussain.dev',
   integrations: [tailwind()]
 });
