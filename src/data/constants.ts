e3f7f64c55c90f14dd42fd2e7d6b174a5a420c52
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-08-28 16:51:51 +0530

    refactor: move constants to separate file

---
+++ src/data/constants.ts
@@ -0,0 +1,42 @@
+export const beginnerResources = [
+    {
+      title: "Introduction to Frappe Framework",
+      link: "https://www.youtube.com/live/HZzialvEmHo?si=SkMAtWruD2pu2ruH",
+    },
+  ];
+
+ export const frappeverseLinks = {
+    products: [
+      { title: "Frappe Framework", link: "https://frappeframework.com" },
+      { title: "ERPNext", link: "https://erpnext.com" },
+      { title: "Frappe Cloud", link: "https://frappecloud.com" },
+    ],
+    goodies: [
+      {
+        title: "The Missing Frappe Manual",
+        link: "https://tmfm.buildwithhussain.dev",
+      },
+      {
+        title: "Bench CLI Cheatsheet",
+        link: "https://hussain.click/bench-cheatsheet",
+      },
+    ],
+    ["Write Ups"]: [
+      {
+        title: "MongoDB Powered DocTypes",
+        link: "https://frappe.io/blog/engineering/mongodb-powered-doctypes",
+      },
+      {
+        title: "Astro Powered Portal Pages in Frappe Apps",
+        link: "https://frappe.io/blog/engineering/astro-powered-web-portal-in-frappe-apps",
+      },
+      {
+        title: "Build An e-Book Store on Frappe",
+        link: "https://frappe.io/blog/engineering/building-an-online-ebook-store-with-frappe-bulma-razorpay-part-I",
+      },
+      {
+        title: "Connect Frappe Backend to your JS Apps!",
+        link: "https://frappe.io/blog/engineering/frappe-in-nodejs-apps-serverless-functions",
+      },
+    ],
+  };
