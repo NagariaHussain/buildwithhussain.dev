533bf7ecb2ac6daa622bf8b07cfd9ba761748f5d
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-09-05 23:38:45 +0530

    feat: sitemaps!

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://buildwithhussain.dev",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "poimandres",
