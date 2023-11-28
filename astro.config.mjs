37a3d37d8b819f1bb298e11bad0146d6dae15215
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-11-28 12:18:42 +0530

    chore: add expressive code plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
-
 import sitemap from "@astrojs/sitemap";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://buildwithhussain.dev",
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [tailwind(), react(), sitemap(), expressiveCode()],
   markdown: {
     shikiConfig: {
       theme: "poimandres",
